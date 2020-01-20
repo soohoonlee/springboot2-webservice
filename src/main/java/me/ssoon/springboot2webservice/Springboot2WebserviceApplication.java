cfaa82e21be02c0e93fcecd825ec8a34525b2207
soohoonlee <jjinimania@gmail.com>
2020-01-20 14:36:44 +0900

    Initial Commit

---
+++ src/main/java/me/ssoon/springboot2webservice/Springboot2WebserviceApplication.java
@@ -0,0 +1,13 @@
+package me.ssoon.springboot2webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2WebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2WebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ssoon/springboot2webservice/Springboot2WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ssoon.springboot2webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
