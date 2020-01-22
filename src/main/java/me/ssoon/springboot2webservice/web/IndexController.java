a4cc8f1b6ab006109fc13b6c917f9b9a258d531a
soohoonlee <jjinimania@gmail.com>
2020-01-22 14:33:58 +0900

    게시글 수정 추가

--- src/main/java/me/ssoon/springboot2webservice/web/IndexController.java
+++ src/main/java/me/ssoon/springboot2webservice/web/IndexController.java
@@ -2,9 +2,11 @@ package me.ssoon.springboot2webservice.web;
 
 import lombok.RequiredArgsConstructor;
 import me.ssoon.springboot2webservice.service.PostsService;
+import me.ssoon.springboot2webservice.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,12 @@ public class IndexController {
   @GetMapping("/posts/save")
   public String postsSave() {
    return "posts-save";
+  }
+
+  @GetMapping("/posts/update/{id}")
+  public String postsUpdate(@PathVariable Long id, Model model) {
+    PostsResponseDto dto = postsService.findById(id);
+    model.addAttribute("post", dto);
+    return "posts-update";
   }
 }
